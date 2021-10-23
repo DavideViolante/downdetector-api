44a2f7388f258802bebf228e01e0f89f1654652a
DavideViolante <fungtq3@gmail.com>
2021-10-23 18:22:47 +0200

    Change from ESLint standard to google, update deps

--- index.js
+++ index.js
@@ -1,56 +1,82 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-function callDowndetector (company, domain) {
-  return axios({
-    method: 'GET',
-    url: `https://downdetector.${domain}/status/${company}/`
-  })
-}
-
-function getScriptContent (data) {
-  const $ = cheerio.load(data)
-  const scriptElem = $('script[type="text/javascript"]')
-  return scriptElem.contents().get(3).data
-}
-
-function getChartPointsString (scriptContent) {
-  return scriptContent.split('\n')
-    .map(line => line.trim())
-    .filter(line => line.includes('{ x: \''))
-}
-
-function getChartPointsObject (chartPoints) {
-  return {
-    reports: str2obj(chartPoints.slice(0, 96)),
-    baseline: str2obj(chartPoints.slice(96, 192))
-  }
-}
-
-function str2obj (chartPoints) {
-  return chartPoints
-    .map(line => line.replace(/\{ | \},|'/g, '').split('x: ').pop().split(', y: '))
-    .map(tuple => ({ date: tuple[0], value: +tuple[1] }))
+/**
+ * Call Downdetector website and get the page content
+ * @param {String} company Company to get the data for
+ * @param {String} domain Domain suffix of downdetector website (eg: com)
+ * @return {String} The page content
+ */
+function callDowndetector(company, domain) {
+  return axios.get(`https://downdetector.${domain}/status/${company}/`);
 }
 
 /**
- *
+ * Get the script tag content from the Downdetector page content
+ * @param {String} data Page content
+ * @return {String} The content of the script tag
+ */
+function getScriptContent(data) {
+  const $ = cheerio.load(data);
+  const scriptElem = $('script[type="text/javascript"]');
+  return scriptElem.contents().get(3).data;
+}
+
+/**
+ * Get array of data from a string
+ * @param {String} scriptContent Script content as a string
+ * @return {Array} Array of strings each one containing a pair of data
+ */
+function getChartPointsString(scriptContent) {
+  return scriptContent.split('\n')
+      .map((line) => line.trim())
+      .filter((line) => line.includes('{ x: \''));
+}
+
+/**
+ * Convert a string to object with reports and baseline properties
+ * @param {String} chartPoints string with dates and values
+ * @return {Object} object with reports and baseline properties
+ */
+function getChartPointsObject(chartPoints) {
+  return {
+    reports: str2obj(chartPoints.slice(0, 96)),
+    baseline: str2obj(chartPoints.slice(96, 192)),
+  };
+}
+
+/**
+ * Convert a string to object with date and value properties
+ * @param {String} chartPoints string to convert to object
+ * @return {Object} object with date and value properties
+ */
+function str2obj(chartPoints) {
+  return chartPoints
+      .map((line) => line
+          .replace(/\{ | \},|'/g, '')
+          .split('x: ').pop()
+          .split(', y: '))
+      .map((tuple) => ({ date: tuple[0], value: +tuple[1] }));
+}
+
+/**
+ * Get data from Downdetector
  * @param {string} company Company name in Downdetector (see URL)
  * @param {string} [domain] Downdetector domain to use (default is 'com')
  */
-async function downdetector (company, domain = 'com') {
+async function downdetector(company, domain = 'com') {
   try {
     if (!company || (typeof company) !== 'string') {
-      throw Error('Invalid input')
+      throw Error('Invalid input');
     }
-    const res = await callDowndetector(company, domain)
-    const scriptContent = getScriptContent(res.data)
-    const chartPoints = getChartPointsString(scriptContent)
-    const { reports, baseline } = getChartPointsObject(chartPoints)
-    return { reports, baseline }
+    const { data } = await callDowndetector(company, domain);
+    const scriptContent = getScriptContent(data);
+    const chartPoints = getChartPointsString(scriptContent);
+    const { reports, baseline } = getChartPointsObject(chartPoints);
+    return { reports, baseline };
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
 }
 
-exports.downdetector = downdetector
+exports.downdetector = downdetector;
--- test/test.js
+++ test/test.js
@@ -1,31 +1,31 @@
-const assert = require('assert')
-const { downdetector } = require('../index')
+const assert = require('assert');
+const { downdetector } = require('../index');
 
 describe('Tests for Downdetector.com unofficial APIs', () => {
   it('should return undefined if input is wrong', async () => {
-    const res1 = await downdetector()
-    assert.strictEqual(res1, undefined)
-    const res2 = await downdetector(123)
-    assert.strictEqual(res2, undefined)
-  })
+    const res1 = await downdetector();
+    assert.strictEqual(res1, undefined);
+    const res2 = await downdetector(123);
+    assert.strictEqual(res2, undefined);
+  });
 
   it('should return correct response', async () => {
-    const { reports, baseline } = await downdetector('steam')
-    assert.ok(reports)
-    assert.ok(baseline)
-    assert.ok(reports.length)
-    assert.ok(baseline.length)
-    assert.strictEqual(reports.length, 96)
-    assert.strictEqual(baseline.length, 96)
-  })
+    const { reports, baseline } = await downdetector('facebook');
+    assert.ok(reports);
+    assert.ok(baseline);
+    assert.ok(reports.length);
+    assert.ok(baseline.length);
+    assert.strictEqual(reports.length, 96);
+    assert.strictEqual(baseline.length, 96);
+  });
 
   it('should return correct response with domain', async () => {
-    const { reports, baseline } = await downdetector('windtre', 'it')
-    assert.ok(reports)
-    assert.ok(baseline)
-    assert.ok(reports.length)
-    assert.ok(baseline.length)
-    assert.strictEqual(reports.length, 96)
-    assert.strictEqual(baseline.length, 96)
-  })
-})
+    const { reports, baseline } = await downdetector('windtre', 'it');
+    assert.ok(reports);
+    assert.ok(baseline);
+    assert.ok(reports.length);
+    assert.ok(baseline.length);
+    assert.strictEqual(reports.length, 96);
+    assert.strictEqual(baseline.length, 96);
+  });
+});
